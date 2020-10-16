f96b3ca601756d2046edc171c9499df4d0f52e83
Yvonne <yvonnersz@gmail.com>
2020-10-16 01:08:37 -0700

    create upvote_post and upvote_comment methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :post
     validates :content, presence: true
+
+    def upvote_comment
+        self.user_id.karma = self.user_id.karma ++
+        self.upvotes = self.upvotes ++ 
+        self.save
+    end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments
     validates :title, :subreddit, :content, presence: true
+
+    def upvote_post
+        self.user_id.karma = self.user_id.karma ++ 
+        self.upvotes = self.upvotes++
+        self.save
+    end
+
 end
