1bc176d0c4b22e62d19ce97d78212622825b4b72
Yvonne <yvonnersz@gmail.com>
2020-10-15 23:40:21 -0700

    namespace api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :comments
+      resources :posts
+      resources :users
+    end
+  end
+  
 end
