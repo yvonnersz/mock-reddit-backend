b12fcb7969d1867e2c58c41f567a06226947d185
Yvonne <yvonnersz@gmail.com>
2020-10-16 02:35:50 -0700

    build CRUD actions for comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::CommentsController < ApplicationController
+    before_action :set_post
+
+    def index
+        comments = @post.comments
+        render json: comments
+    end
+
+    def create
+        comment = @post.comments.create(comment_params)
+        render json: comment
+    end
+
+    def show
+        comment = @post.comments.find_by(:id => params[:id])
+        render json: comment
+    end
+
+    def destroy
+        comment = @post.comments.find_by(:id => params[:id])
+        comment.destroy
+    end
+
+    private
+
+    def set_post
+        @post = Post.find_by(:id => params[:post_id])
+    end
+    
+    def comment_params
+        params.require(:comment).permit(:content, :upvotes)
+    end
+
 end
