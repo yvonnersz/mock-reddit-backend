ed8f41ac136f9cd796d8c87d0e1f9b757ef3956e
Yvonne <yvonnersz@gmail.com>
2020-10-16 02:18:19 -0700

    build CRUD actions for posts controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::PostsController < ApplicationController
+
+    def index
+        posts = Post.all
+        render json: posts
+    end
+
+    def create
+        post = Post.create(post_params)
+        render json: post
+    end
+
+    def show
+        post = Post.find_by(:id => params[:id])
+        render json: post
+    end
+
+    def destroy
+        post = Post.find_by(:id => params[:id])
+        post.destroy
+        render json: post
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :subreddit, :content, :upvotes)
+    end
+
 end
