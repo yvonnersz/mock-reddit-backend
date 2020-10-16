42fd64748a4cf59ee6caa5ac9f9e5c8f2b72ab40
Yvonne <yvonnersz@gmail.com>
2020-10-16 02:24:39 -0700

    build CRUD actions for users controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -18,7 +18,6 @@ class Api::V1::PostsController < ApplicationController
     def destroy
         post = Post.find_by(:id => params[:id])
         post.destroy
-        render json: post
     end
 
     private
