2b4a812fcc82669011b96c8e34326efc3f61cd6b
Yvonne <yvonnersz@gmail.com>
2020-10-15 23:48:32 -0700

    add title attribute to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :comments
-    validates :subreddit, :content, presence: true
+    validates :title, :subreddit, :content, presence: true
 end
--- db/migrate/20201016060534_create_posts.rb
+++ db/migrate/20201016060534_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
+      t.string :title
       t.string :subreddit
       t.string :content
       t.integer :upvotes
