9b7296bc7955dacf1a30f46f9bb8b73ab19f2888
Yvonne <yvonnersz@gmail.com>
2020-10-16 06:53:53 -0700

    build new serializer for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::UsersController < ApplicationController
 
     def show
         user = User.find_by(:id => params[:id])
-        render json: user
+        render json: UserSerializer.new(user)
     end
 
     def destroy
