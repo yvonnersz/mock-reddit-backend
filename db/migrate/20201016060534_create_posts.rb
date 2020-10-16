5267c43eb7907a7e77f082e4f91de441bb4d6df9
Yvonne <yvonnersz@gmail.com>
2020-10-15 23:08:42 -0700

    add associations to User, Post, and Comment

---
+++ db/migrate/20201016060534_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :subreddit
+      t.string :content
+      t.integer :upvotes
+
+      t.timestamps
+    end
+  end
+end
